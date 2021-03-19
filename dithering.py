5325301ca54d397a79b2344accdaa25d95b8f04c
Shayan Tehrani <shayanthrn@gmail.com>
2021-03-19 14:29:03 +0330

    writing basics

--- dithering.py
+++ dithering.py
@@ -2,8 +2,19 @@ from PIL import Image
 
 
 im = Image.open("dog.jpg").convert('L')
-pixels= im.load()
+orginal_pixels = im.load()
+dithered_im=Image.new("L",(im.size[0]*2,im.size[1]*2),"black")
+dithered_pixels = dithered_im.load()
+
+# using [[0 2], as sliding window
+#        [3 1]]
+
+slidingwindow=[[0,2],[3,1]]
+
 for i in range(im.size[0]):
     for j in range(im.size[1]):
-        pixels[i,j]=250
-im.show()
+        mappedvalue=orginal_pixels[i,j]/64     #maps 0-255 to [0-4)
+        if(mappedvalue>slidingwindow[0][0]):   #check threshhold for index 0,0 of sliding window
+            dithered_pixels[i*2,j*2]=255
+
+dithered_im.show()
